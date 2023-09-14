3c215ea9fa5b14afb5600406b2fc5617d9e5e139
sriya-nimma <sriya.nimma@utexas.edu>
2023-09-14 17:33:23 -0500

    added commit

---
+++ volume_of_sphere.py
@@ -0,0 +1,7 @@
+def calculate_volume_of_sphere(radius):
+    volume = (4/3)*3.14 * (radius ** 3)
+    print(volume)
+
+if __name__ == "__main__":
+    calculate_volume_of_sphere(30)
+    calculate_volume_of_sphere(40)
